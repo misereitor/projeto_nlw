14943ed3004cef0db21206a88a723d252cadd129
Misael Miranda <66282432+misereitor@users.noreply.github.com>
2021-04-20 12:27:23 -0300

    dia 2 finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from "express"
+import "./database"
+import { routes } from './routes'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    return res.send('Olá Mundo!')
-})
-
-app.post('/', (req, res) => {
-    return res.json({ message: "Usuário cadastrado com sucesso!" })
-})
+app.use(routes)
 
 app.listen(8080, () => console.log('Server is running on port 8080'))
